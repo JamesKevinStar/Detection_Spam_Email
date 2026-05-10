3af0caac2aea8a9725adaeccea0f7810798c2f7b
JamesKevinStar <jameskevinstar.1@gmail.com>
2026-05-09 19:55:11 -0500

    style: change registry.py

--- registry.py
+++ registry.py
@@ -1,10 +1,10 @@
 import mlflow
 
-if __name__ == "__main__":
-    # Register models
-    mlflow.set_tracking_uri("http://127.0.0.1:8080/")
-    best_model_name = "Logistic Regresion"
-    run_id = "ceed84ea0c6a40afb8be2b05e89137f3"
-    model_uri = "runs:/{}/{}".format(run_id, best_model_name)
+# Register models
+mlflow.set_tracking_uri("http://127.0.0.1:8080/")
 
-    mlflow.register_model(model_uri, best_model_name)
+best_model_name = "Logistic Regresion"
+run_id = "ceed84ea0c6a40afb8be2b05e89137f3"
+model_uri = "runs:/{}/{}".format(run_id, best_model_name)
+
+mlflow.register_model(model_uri, best_model_name)
